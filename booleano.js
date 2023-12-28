1071d1114dc79696293927c6ddd168e0c6fc9518
João Augusto <lacerdajoao2002@gmail.com>
2023-12-28 14:57:59 -0400

    alteraçao nome

--- booleano.js
+++ booleano.js
@@ -1,5 +1,8 @@
 const primeiroNumero = 5;
 const segundoNumero = 10;
 const terceiroNumero = 10;
+let cadastroAtivo = true;
+
 console.log(primeiroNumero === segundoNumero);
 console.log(segundoNumero === terceiroNumero);
+
